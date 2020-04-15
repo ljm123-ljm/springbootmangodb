2dea1c494d61fd33d142bd979718d2d5638f20c6
ljm <1914660874@qq.com>
2020-04-15 20:11:35 +0800

    上传springboot整合mangodb

---
+++ springbootandmangodb/src/test/java/com/ljm/User.java
@@ -0,0 +1,16 @@
+package com.ljm;/**
+ * @Author LJM
+ * @Date 2020/4/15 20:07
+ * @Version 1.0
+ */
+
+/**
+ * @ClassName User
+ * @Description 创建一个新的User类
+ * @Author ljm
+ * @Date 20:07 2020/4/15
+ * @Version 2.1
+ **/
+public class User {   private String username;
+    private Integer id;
+}
